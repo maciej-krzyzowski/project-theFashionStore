d0a46dd470ff6ccafdb8b6a12737e950a9137913
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-02-18 16:51:58 +0100

    Add carousel and fix code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let i = 0;
+// let i = 0;
 
 function button() {
     const hamburgerBtn = document.querySelector('.hamburger__wrapper');
@@ -17,52 +17,114 @@ function button() {
 
 button();
 
-function toggleClass() {
-    const caruselElements = document.querySelectorAll('.carusel__item');
-    for (let element of caruselElements) {
-        element.classList.remove('carusel__item--active');
-    }
-    caruselElements[i].classList.add('carusel__item--active');
-};
+// function toggleClass() {
+//     const caruselElements = document.querySelectorAll('.carusel__item');
+//     for (let element of caruselElements) {
+//         element.classList.remove('carusel__item--active');
+//     }
+//     caruselElements[i].classList.add('carusel__item--active');
+// };
 
-function carusel() {
-    if (i >= 0 && i < 2) {
+// function carusel() {
+//     if (i >= 0 && i < 2) {
+//         i++;
+//         toggleClass();
+//     } else if (i >= 2) {
+//         i = 0;
+//         toggleClass();
+//     }
+// }
+
+// // setInterval(function () {
+// //     console.log('dupa');
+// //     carusel();;
+// // }, 4000);
+
+// function caruselButton() {
+//     const btnLeft = document.querySelector('.btn__carusel--left');
+//     const btnRight = document.querySelector('.btn__carusel--right')
+
+//     btnLeft.addEventListener('click', () => {
+//         if (i >= 1 && i <= 2) {
+//             i--;
+//             toggleClass();
+//         } else {
+//             i = 2;
+//             toggleClass();
+//         }
+//     });
+
+//     btnRight.addEventListener('click', () => {
+//         if (i >= 0 && i <= 1) {
+//             i++;
+//             toggleClass();
+//         } else {
+//             i = 0;
+//             toggleClass();
+//         }
+//     });
+// }
+
+// caruselButton();
+
+const carusel = () => {
+    const carusel = document.querySelector('.carusel__wrapper')
+    const slides = Array.from(carusel.children);
+    const nextButton = document.querySelector('.btn__carusel--right');
+    const prevButton = document.querySelector('.btn__carusel--left');
+    let i = 0;
+
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    const slidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + 'px';
+    }
+    slides.forEach(slidePosition);
+
+    const changeRight = () => {
+        carusel.style.transform = carusel.style.transform + 'translateX(-100vw)';
         i++;
-        toggleClass();
-    } else if (i >= 2) {
-        i = 0;
-        toggleClass();
+        if (i === slides.length) {
+            carusel.style.transform = 'translate(0)';
+            i = 0
+        }
     }
-}
-
-// setInterval(function () {
-//     console.log('dupa');
-//     carusel();;
-// }, 4000);
-
-function caruselButton() {
-    const btnLeft = document.querySelector('.btn__carusel--left');
-    const btnRight = document.querySelector('.btn__carusel--right')
-
-    btnLeft.addEventListener('click', () => {
-        if (i >= 1 && i <= 2) {
-            i--;
-            toggleClass();
-        } else {
+    const changeLeft = () => {
+        carusel.style.transform = carusel.style.transform + 'translateX(100vw)';
+        i--;
+        if (i === -1) {
+            carusel.style.transform = 'translate(-' + (slides.length - 1) * 100 + 'vw)';
             i = 2;
-            toggleClass();
         }
-    });
+    }
 
-    btnRight.addEventListener('click', () => {
-        if (i >= 0 && i <= 1) {
-            i++;
-            toggleClass();
-        } else {
-            i = 0;
-            toggleClass();
+    const caruselInterval = () => {
+
+        const allClearIterval = () => {
+            for (var i = 1; i < 99999; i++)
+                window.clearInterval(i);
         }
-    });
+
+        allClearIterval();
+
+        const interval = () => setInterval(() => {
+            changeRight();
+        }, 3000);
+
+        interval();
+    }
+
+    caruselInterval();
+
+    nextButton.addEventListener('click', () => {
+        changeRight();
+        caruselInterval();
+    })
+
+    prevButton.addEventListener('click', () => {
+        changeLeft();
+        caruselInterval();
+    })
 }
 
-caruselButton();
+carusel();
