7d15bd9255069eace43363b1086382abf3d32506
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-02-19 00:41:56 +0100

    Add smallCarusel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// let i = 0;
-
-function button() {
+const button = () => {
     const hamburgerBtn = document.querySelector('.hamburger__wrapper');
     const menuWrapper = document.querySelector('.menu__wrapper')
     const searchBtn = document.querySelector('.btn__search')
@@ -14,58 +12,7 @@ function button() {
         searchWrapper.classList.toggle('navbar__form--active')
     })
 }
-
 button();
-
-// function toggleClass() {
-//     const caruselElements = document.querySelectorAll('.carusel__item');
-//     for (let element of caruselElements) {
-//         element.classList.remove('carusel__item--active');
-//     }
-//     caruselElements[i].classList.add('carusel__item--active');
-// };
-
-// function carusel() {
-//     if (i >= 0 && i < 2) {
-//         i++;
-//         toggleClass();
-//     } else if (i >= 2) {
-//         i = 0;
-//         toggleClass();
-//     }
-// }
-
-// // setInterval(function () {
-// //     console.log('dupa');
-// //     carusel();;
-// // }, 4000);
-
-// function caruselButton() {
-//     const btnLeft = document.querySelector('.btn__carusel--left');
-//     const btnRight = document.querySelector('.btn__carusel--right')
-
-//     btnLeft.addEventListener('click', () => {
-//         if (i >= 1 && i <= 2) {
-//             i--;
-//             toggleClass();
-//         } else {
-//             i = 2;
-//             toggleClass();
-//         }
-//     });
-
-//     btnRight.addEventListener('click', () => {
-//         if (i >= 0 && i <= 1) {
-//             i++;
-//             toggleClass();
-//         } else {
-//             i = 0;
-//             toggleClass();
-//         }
-//     });
-// }
-
-// caruselButton();
 
 const carusel = () => {
     const carusel = document.querySelector('.carusel__wrapper')
@@ -126,5 +73,116 @@ const carusel = () => {
         caruselInterval();
     })
 }
-
 carusel();
+
+const smallCarusel = () => {
+
+    const menSmallCarusel = () => {
+        const caruselMen = document.querySelector('.small-carusel-men');
+        const slidesMen = Array.from(caruselMen.children)
+        const menButtonWrapper = document.querySelector('.products__buttons--men')
+        const menNextButton = menButtonWrapper.querySelector('.btn__small-carusel--right');
+        const menPrevButton = menButtonWrapper.querySelector('.btn__small-carusel--left');
+        const menActive = 'small-carusel__men--active';
+        let i = 0;
+
+        menNextButton.addEventListener('click', () => {
+            if (i == 0 || i < slidesMen.length - 1) {
+                slidesMen[i].classList.remove(menActive);
+                i++;
+                slidesMen[i].classList.add(menActive);
+            } else {
+                slidesMen[i].classList.remove(menActive);
+                i = 0;
+                slidesMen[i].classList.add(menActive);
+            }
+        });
+
+        menPrevButton.addEventListener('click', () => {
+            if (i > 0 && i <= slidesMen.length - 1) {
+                slidesMen[i].classList.remove(menActive);
+                i--;
+                slidesMen[i].classList.add(menActive);
+            } else {
+                slidesMen[i].classList.remove(menActive);
+                i = slidesMen.length - 1;
+                slidesMen[i].classList.add(menActive);
+            }
+        });
+    };
+
+    const womenSmallCarusel = () => {
+
+        const caruselWomen = document.querySelector('.small-carusel-women');
+        const slidesWomen = Array.from(caruselWomen.children)
+        const womenButtonWrapper = document.querySelector('.products__buttons--women')
+        const womenNextButton = womenButtonWrapper.querySelector('.btn__small-carusel--right');
+        const womenPrevButton = womenButtonWrapper.querySelector('.btn__small-carusel--left');
+        const womenActive = 'small-carusel__women--active';
+        let i = 0;
+
+        womenNextButton.addEventListener('click', () => {
+            if (i == 0 || i < slidesWomen.length - 1) {
+                slidesWomen[i].classList.remove(womenActive);
+                i++;
+                slidesWomen[i].classList.add(womenActive);
+            } else {
+                slidesWomen[i].classList.remove(womenActive);
+                i = 0;
+                slidesWomen[i].classList.add(womenActive);
+            }
+        });
+
+        womenPrevButton.addEventListener('click', () => {
+            if (i > 0 && i <= slidesWomen.length - 1) {
+                slidesWomen[i].classList.remove(womenActive);
+                i--;
+                slidesWomen[i].classList.add(womenActive);
+            } else {
+                slidesWomen[i].classList.remove(womenActive);
+                i = slidesWomen.length - 1;
+                slidesWomen[i].classList.add(womenActive);
+            }
+        });
+    };
+
+    const cardsSmalCarusel = () => {
+        const slidesCards = document.querySelectorAll('.small-carusel__cards');
+        console.log(slidesCards.length);
+        const cardsButtonWrapper = document.querySelector('.cards__button');
+        const cardsNextButton = cardsButtonWrapper.querySelector('.btn__small-carusel--right');
+        const cardsPrevButton = cardsButtonWrapper.querySelector('.btn__small-carusel--left');
+        const cardsActive = 'small-carusel__cards--active';
+        let i = 0;
+
+        cardsNextButton.addEventListener('click', () => {
+            if (i == 0 || i < slidesCards.length - 1) {
+                slidesCards[i].classList.remove(cardsActive);
+                i++;
+                slidesCards[i].classList.add(cardsActive);
+            } else {
+                slidesCards[i].classList.remove(cardsActive);
+                i = 0;
+                slidesCards[i].classList.add(cardsActive);
+            }
+        });
+
+        cardsPrevButton.addEventListener('click', () => {
+            if (i > 0 && i <= slidesCards.length - 1) {
+                slidesCards[i].classList.remove(cardsActive);
+                i--;
+                slidesCards[i].classList.add(cardsActive);
+            } else {
+                slidesCards[i].classList.remove(cardsActive);
+                i = slidesCards.length - 1;
+                slidesCards[i].classList.add(cardsActive);
+            }
+        });
+    }
+
+    menSmallCarusel();
+    womenSmallCarusel();
+    cardsSmalCarusel();
+}
+
+smallCarusel();
