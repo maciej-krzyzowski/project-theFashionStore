46eecd7e96dd20c0308ca5e5adc3bb25d9cb482e
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-06-02 19:57:11 +0200

    change carusel

--- js/script.js
+++ js/script.js
@@ -1,75 +1,81 @@
-// BUTTON 
+// BUTTON
 
 const button = () => {
-    const hamburgerBtn = document.querySelector('.hamburger__wrapper');
-    const menuWrapper = document.querySelector('.menu__wrapper');
-    const searchBtn = document.querySelector('.btn__search');
-    const searchWrapper = document.querySelector('.navbar__form');
+    const hamburgerBtn = document.querySelector(".hamburger__wrapper");
+    const menuWrapper = document.querySelector(".menu__wrapper");
+    const searchBtn = document.querySelector(".btn__search");
+    const searchWrapper = document.querySelector(".navbar__form");
 
-    hamburgerBtn.addEventListener('click', () => {
-        menuWrapper.classList.toggle('menu__wrapper--active');
+    hamburgerBtn.addEventListener("click", () => {
+        menuWrapper.classList.toggle("menu__wrapper--active");
     });
 
-    searchBtn.addEventListener('click', () => {
-        searchWrapper.classList.toggle('navbar__form--active')
+    searchBtn.addEventListener("click", () => {
+        searchWrapper.classList.toggle("navbar__form--active");
     });
 };
 
 button();
 
-// CARUSEL 
+// CARUSEL
 
 const carusel = () => {
-    let i = 0;
-    const carusel = document.querySelector('.carusel__wrapper');
+    const carusel = document.querySelector(".carusel__wrapper");
     const slides = Array.from(carusel.children);
-    const nextButton = document.querySelector('.btn__carusel--right');
-    const prevButton = document.querySelector('.btn__carusel--left');
-    const slideWidth = slides[0].getBoundingClientRect().width;
+    const nextButton = document.querySelector(".btn__carusel--right");
+    const prevButton = document.querySelector(".btn__carusel--left");
+    let i = 1;
 
-    const slidePosition = (slide, index) => {
-        slide.style.left = slideWidth * index + 'px';
+    const changeSlide = (index) => {
+        let prev = index - 1;
+        if (prev === -1) prev = slides.length - 1;
+
+        let next = index + 1;
+        if (next === slides.length) next = 0;
+
+        for (let k = 0; k < slides.length; k++) {
+            slides[k].classList.remove("carusel__item--prev");
+            slides[k].classList.remove("carusel__item--active");
+            slides[k].classList.remove("carusel__item--next");
+        }
+        slides[prev].classList.add("carusel__item--prev");
+        slides[i].classList.add("carusel__item--active");
+        slides[next].classList.add("carusel__item--next");
     };
-    slides.forEach(slidePosition);
 
     const changeRight = () => {
-        carusel.style.transform = carusel.style.transform + 'translateX(-100vw)';
         i++;
-        if (i === slides.length) {
-            carusel.style.transform = 'translate(0)';
-            i = 0;
-        };
+        if (i === slides.length) i = 0;
+        changeSlide(i);
     };
 
     const changeLeft = () => {
-        carusel.style.transform = carusel.style.transform + 'translateX(100vw)';
         i--;
-        if (i === -1) {
-            carusel.style.transform = 'translate(-' + (slides.length - 1) * 100 + 'vw)';
-            i = 2;
-        };
+        if (i === -1) i = slides.length - 1;
+        changeSlide(i);
     };
 
-    const allClearInterval = () => {
-        for (var i = 1; i < 99999; i++) {
-            window.clearInterval(i);
-        }
-    }
-
-    const interval = () => setInterval(() => {
-        changeRight();
-    }, 3500);
+    const interval = () =>
+        setInterval(() => {
+            changeRight();
+        }, 2500);
 
     interval();
 
-    nextButton.addEventListener('click', () => {
-        allClearInterval();
+    const clearInterval = () => {
+        for (var i = 1; i < 99999; i++) {
+            window.clearInterval(i);
+        }
+    };
+
+    nextButton.addEventListener("click", () => {
+        clearInterval();
         changeRight();
         interval();
     });
 
-    prevButton.addEventListener('click', () => {
-        allClearInterval();
+    prevButton.addEventListener("click", () => {
+        clearInterval();
         changeLeft();
         interval();
     });
@@ -77,16 +83,16 @@ const carusel = () => {
 
 carusel();
 
-// SMALLCARUSEL 
+// SMALLCARUSEL
 
 const smallCarusel = (carusel, buttonWrapper) => {
     let i = 0;
-    const slides = Array.from(carusel.children)
-    const nextButton = buttonWrapper.querySelector('.btn__small-carusel--right');
-    const prevButton = buttonWrapper.querySelector('.btn__small-carusel--left');
-    const active = 'small-carusel__active';
+    const slides = Array.from(carusel.children);
+    const nextButton = buttonWrapper.querySelector(".btn__small-carusel--right");
+    const prevButton = buttonWrapper.querySelector(".btn__small-carusel--left");
+    const active = "small-carusel__active";
 
-    nextButton.addEventListener('click', () => {
+    nextButton.addEventListener("click", () => {
         if (i == 0 || i < slides.length - 1) {
             slides[i].classList.remove(active);
             i++;
@@ -98,7 +104,7 @@ const smallCarusel = (carusel, buttonWrapper) => {
         }
     });
 
-    prevButton.addEventListener('click', () => {
+    prevButton.addEventListener("click", () => {
         if (i > 0 && i <= slides.length - 1) {
             slides[i].classList.remove(active);
             i--;
@@ -109,26 +115,25 @@ const smallCarusel = (carusel, buttonWrapper) => {
             slides[i].classList.add(active);
         }
     });
-}
+};
 
 const menCarusel = () => {
-    const carusel = document.querySelector('.small-carusel-men');
-    const menButtonWrapper = document.querySelector('.products__buttons--men');
+    const carusel = document.querySelector(".small-carusel-men");
+    const menButtonWrapper = document.querySelector(".products__buttons--men");
 
     smallCarusel(carusel, menButtonWrapper);
 };
 
 const womenCarusel = () => {
-
-    const carusel = document.querySelector('.small-carusel-women');
-    const womenButtonWrapper = document.querySelector('.products__buttons--women');
+    const carusel = document.querySelector(".small-carusel-women");
+    const womenButtonWrapper = document.querySelector(".products__buttons--women");
 
     smallCarusel(carusel, womenButtonWrapper);
 };
 
 const cardsCarusel = () => {
-    const carusel = document.querySelector('.small-carusel-cards');
-    const cardsButtonWrapper = document.querySelector('.cards__button');
+    const carusel = document.querySelector(".small-carusel-cards");
+    const cardsButtonWrapper = document.querySelector(".cards__button");
 
     smallCarusel(carusel, cardsButtonWrapper);
 };
